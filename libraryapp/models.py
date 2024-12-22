956bbfe34c3868a801b9c5092f7f5e1ce979f530
Max Salautin <maksal2005@gmail.com>
2024-12-22 19:28:28 +0200

    Fix

--- libraryapp/models.py
+++ libraryapp/models.py
@@ -1,21 +1,21 @@
 from django.db import models
-from .mixins import TimeStampedUUIDModel
+from .mixins import BaseModelMixin
 
 # Create your models here.
 
-class Author(TimeStampedUUIDModel):
+class Author(BaseModelMixin):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
-class Genre(TimeStampedUUIDModel):
+class Genre(BaseModelMixin):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
-class Book(TimeStampedUUIDModel):
+class Book(BaseModelMixin):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
     genre = models.ForeignKey(Genre, on_delete=models.SET_DEFAULT, default="Unknown")
