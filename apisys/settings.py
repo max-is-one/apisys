041d5d83b8434bd1f8ff0b3968132e62388e7e57
Max Salautin <maksal2005@gmail.com>
2024-12-30 17:00:32 +0200

    DB settings

--- apisys/settings.py
+++ apisys/settings.py
@@ -110,7 +110,7 @@ WSGI_APPLICATION = 'apisys.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-#'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -121,15 +121,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-#'''
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-'''
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
